0ee50b9e392c6b6a525a484bcb91ae982016d3a2
Elisa Giorgi <elisa.giorgi.94@gmail.com>
2021-12-01 23:35:51 +0100

    feat: add spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,8 @@
 import React from "react";
+import ReactLoading from "react-loading";
 
-const Loading = () => {
-  return <div>Loading...</div>;
-};
+const Loading = ({ type, color }) => (
+  <ReactLoading type={type} color={color} height={"20%"} width={"20%"} />
+);
 
 export default Loading;
